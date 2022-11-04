95c6e951bc5b6da16ad4633b541b06c79f3e480a
Anatoly Scherbakov <altaisoft@gmail.com>
2022-11-05 00:40:29 +0400

    Draft implementation

---
+++ jeeves.py
@@ -0,0 +1,8 @@
+def test__foo():
+    """This is a test nested function."""
+    print('test!!')
+
+
+def bazinga():
+    """test function"""
+    print('boo')
--- jeeves_core/cli.py
+++ jeeves_core/cli.py
@@ -1,12 +1,26 @@
-from importlib.metadata import entry_points
-from typing import Tuple
+import importlib.util
+import sys
+from collections import defaultdict
+from pathlib import Path
+from typing import Tuple, Callable, Iterable
 
-from benedict import benedict
-from boltons.iterutils import remap
-from rich.traceback import install
 from typer import Typer
 
-from jeeves_core.errors import InvalidCommand, NoPluginsInstalled
+from jeeves_core.errors import InvalidCommand
+
+
+def import_module_by_path(path: Path):
+    """
+    Import module knowing its filesystem path.
+
+    Source: https://stackoverflow.com/a/67692/1245471
+    """
+    module_name = str(path).strip('/').replace('.', '_').replace('/', '.')
+    spec = importlib.util.spec_from_file_location(module_name, path)
+    foo = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = foo
+    spec.loader.exec_module(foo)
+    return foo
 
 
 def remap_enter(_path: Tuple[str, ...], key: str, key_value: object):
@@ -45,29 +59,56 @@ def remap_exit(
     return new_parent
 
 
+def collect_commands(path: Path) -> Iterable[Tuple[str, Callable]]:
+    for directory in [path, *path.parents]:
+        jeeves_file = directory / 'jeeves.py'
+
+        if jeeves_file.is_file():
+            jeeves_module = import_module_by_path(jeeves_file)
+
+            for name, python_object in vars(jeeves_module).items():
+                if callable(python_object):
+                    yield name, python_object
+
+
+def recursive_dict():
+    return defaultdict(recursive_dict)
+
+
+def app_from_tree(tree):
+    root_app = Typer()
+
+    for key, value in tree.items():
+        if isinstance(value, dict):
+            root_app.add_typer(
+                app_from_tree(value),
+                name=key,
+            )
+
+        else:
+            root_app.command(name=key)(value)
+
+    return root_app
+
+
 def app() -> None:
-    """Construct a Typer app from installed Jeeves plugins."""
-    install(show_locals=False)
+    current_path = Path.cwd()
+    names_and_commands = collect_commands(current_path)
+    sequences_and_commands = [
+        (name.split('__'), command)
+        for name, command in names_and_commands
+    ]
 
-    try:
-        points = entry_points()['jeeves']
-    except KeyError as err:
-        raise NoPluginsInstalled() from err
+    tree = recursive_dict()
 
-    if not points:
-        raise NoPluginsInstalled()
+    for sequence, command in sequences_and_commands:
+        root = tree
+        for segment in sequence[:-1]:
+            root = root[segment]
 
-    commands_tree = benedict()
-    for point in points:
-        commands_tree[point.name] = point.load()
+        root[sequence[-1]] = command
 
-    typer_app = remap(
-        root=commands_tree,
-        enter=remap_enter,
-        exit=remap_exit,
-    )
-
-    return typer_app()
+    return app_from_tree(tree)()
 
 
 if __name__ == '__main__':
