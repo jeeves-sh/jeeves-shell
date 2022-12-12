c141700258460ff7bab2b888434be2edd8cc966b
Anatoly Scherbakov <altaisoft@gmail.com>
2022-12-12 17:46:42 +0400

    Get rid of `mypy` section in `setup.cfg`

--- jeeves.py
+++ jeeves.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from typing import List
+from typing import List, Iterable
 
 import more_itertools
 from plumbum.cmd import poetry, isort
@@ -20,11 +20,42 @@ def _python_directories() -> List[Path]:
     ]
 
 
+def _construct_mypy_flags() -> Iterable[str]:
+    """
+    Mypy configuration.
+
+    - http://bit.ly/2zEl9WI
+    - Source: wemake-python-package.
+    """
+    yield '--disallow-redefinition'
+    yield '--check-untyped-defs'
+    yield '--disallow-any-explicit'
+    yield '--disallow-any-generics'
+    yield '--disallow-untyped-calls'
+    yield '--ignore-missing-imports'
+    yield '--implicit-reexport'
+    yield '--local-partial-types'
+    yield '--strict-optional'
+    yield '--strict-equality'
+    yield '--no-implicit-optional'
+    yield '--warn-no-return'
+    yield '--warn-unused-ignores'
+    yield '--warn-redundant-casts'
+    yield '--warn-unused-configs'
+    yield '--warn-unreachable'
+
+
 def lint():
     """Lint code."""
     directories = _python_directories()
 
-    run('mypy', *directories, **kwargs)
+    run(
+        'mypy',
+        *directories,
+        *_construct_mypy_flags(),
+        **kwargs,
+    )
+
     run('flakeheaven', 'lint', *directories, **kwargs)
 
     poetry('check', **kwargs)
